6257f2c1f2cdf65cd767c20399dc58eabe3c5a03
JShin <shin.jihyung@gmail.com>
2021-11-09 11:38:11 -0500

    modified Nightingale_column_name_matching.r

--- UKB_Nightingale/R/Nightingale_column_name_matching.r
+++ UKB_Nightingale/R/Nightingale_column_name_matching.r
@@ -7,9 +7,6 @@
 # Ran once: Nightingale metabolite metadata
 # install.packages("remotes")
 # remotes::install_github("NightingaleHealth/ggforestplot")
-# NG_biomarker_metadata = (ggforestplot::df_NG_biomarker_metadata)
-# NG_biomarker_metadata = NG_biomarker_metadata %>% dplyr::select(-units)
-# write_tsv(NG_biomarker_metadata ,"NG_biomarker_metadata.tsv")
 # 
 # output: 'NG_UKB_mathced_metaboIDs.tsv'
 ###############################################################################
@@ -26,25 +23,27 @@ x = c('stringr','tidyverse','dplyr','readxl',
 
 lapply(x,require,character.only=T);rm(x)
 
-wd="/Users/jshin/OneDrive - SickKids/ukbb_LDL.D_gwas/scripts/ukbb_gwas_LDL.D/R"
+wd="/Users/jshin/OneDrive - SickKids/neuroCHARGE_BrainMS_CircMetabolism/scripts/neuroCHARGE/UKB_Nightingale"
 setwd(wd)
 
 #------------------------------------------------------------------------------
 # read in table files
-ukb_metabo_vars = read_xlsx('../../../DataVariableList.xlsx',sheet=2)
+ukb_metabo_vars = read_xlsx('../UKB_Nightingale/metadata/DataVariableList.xlsx',sheet=2)
 ukb_metabo_vars = ukb_metabo_vars %>% 
   mutate(tmpID = str_remove(tolower(Description)," percentage")) %>% 
   mutate(tmpID = str_remove(tmpID," ratio ")) %>% 
   mutate(tmpID = str_remove(tmpID," ratio"))
 
-NG_metabo_vars = fread('../NG_biomarker_metadata.tsv')
-NG_metabo_vars = NG_metabo_vars %>% 
+NG_biomarker_metadata = (ggforestplot::df_NG_biomarker_metadata)
+NG_biomarker_metadata = NG_biomarker_metadata %>% dplyr::select(-unit)
+
+NG_biomarker_metadata = NG_biomarker_metadata %>% 
   mutate(tmpID = tolower(description)) %>%
   mutate(tmpID = str_remove(tmpID,"ratio of ")) %>% 
   mutate(tmpID = str_remove(tmpID,"ratio "))
   
-setdiff(ukb_metabo_vars$tmpID,NG_metabo_vars$tmpID)
-intersect(names(ukb_metabo_vars),names(NG_metabo_vars))
+setdiff(ukb_metabo_vars$tmpID,NG_biomarker_metadata$tmpID)
+intersect(names(ukb_metabo_vars),names(NG_biomarker_metadata))
 
-ukb_metabo_vars = merge(ukb_metabo_vars,NG_metabo_vars)
-write_tsv(ukb_metabo_vars,"../NG_UKB_mathced_metaboIDs.tsv")          
+ukb_metabo_vars = merge(ukb_metabo_vars,NG_biomarker_metadata)
+write_tsv(ukb_metabo_vars,"metadata/NG_UKB_mathced_metaboIDs.tsv")          
